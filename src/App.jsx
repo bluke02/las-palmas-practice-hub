f75120eca6c733bb7bee4cd97d254db3a32c4140
bluke02 <bluke02@yahoo.com>
2026-09-12 12:12:56 -0700

    Format App.jsx for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-import {Routes,Route} from 'react-router-dom';import Home from './pages/Home';export default function App(){return <Routes><Route path='/' element={<Home/>}/></Routes>} 
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
